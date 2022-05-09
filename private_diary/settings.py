70f12c7ae82cb0114bb544ae7df8e0e9de6c3bd5
daiki <gigabowser@hotmail.co.jp>
2022-05-09 09:25:23 +0900

    本番環境用設定の修正

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -52,7 +52,7 @@ LOGGING = {
 
     # フォーマッタの設定
     'formatters': {
-        'dev': {
+        'prod': {
             'format': '\t'.join([
                 '%(asctime)s',
                 '[%(levelname)s]',
