fa90c771524a0a9f44f6610e596274f5a4a120c8
daiki <gigabowser@hotmail.co.jp>
2022-05-10 19:57:58 +0900

    本番環境でなぜか繋がらないため、DBホストを環境変数で設定するように修正

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'private_diary',
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '',
     }
 }
